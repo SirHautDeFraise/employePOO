8bc9d84822b0d0d34d7cf4d1eed7e43dd37c0b37
Thibault Laude <laudethibault_pro@outlook.fr>
2020-10-08 10:25:27 +0200

    Partie 2 fini

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!--
+Page index.php fait par LAUDE Thibault
+-->
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title></title>
+  </head>
+  <body>
+    <p>Salut</p>
+    <?php
+    // put your code here uwu
+    ?>
+  </body>
+</html>
