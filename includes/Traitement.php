1a11e465aeec427ff76667eabe98a6260cbc8981
Thibault Laude <laudethibault_pro@outlook.fr>
2020-10-13 20:50:49 +0200

    Modification de Traitement et index avec les classes

--- includes/Traitement.php
+++ includes/Traitement.php
@@ -8,25 +8,44 @@
  */
 class Traitement {
 
-  public static function instanciationUnEmploye(): void {
-    $e = new employe(1, "Dupont", "Jacques", new DateTime("12/07/1980"), 1000.00);
-    echo $e;
-    
-  }
-  
-  public static function instanciationUnEmployeErreur(): void {
-    $e = new employe(2, "Durand", "Sulvie", new DateTime("1975/06/14"), 800.00);
-    echo $e;
-    
-  }
-  
-  public static function instanciationProjetV1(): void {
-    $p = new Projet('PR7', 'Librairie Gelistout', 102);
-    echo $p ;
-    echo "Nom du projet = " . $p->getNomProjet();
-  }
+    public static function instanciationUnEmploye(): void {
+        $e = new Employemploye(1, "Dupont", "Jacques", new DateTime("12/07/1980"), 1000.00);
+        echo $e;
+    }
 
-  /* ->getNom();
-    echo "l'employé instancié s'appelle " . $nom;
-   */
+    public static function instanciationUnEmployeErreur(): void {
+        $e = new Employe(1, "Dupont", "Jacques", new DateTime("12/07/1980"), 800.00);
+        echo $e;
+    }
+
+    public static function instanciationProjetV1(): void {
+        $p = new Projet('PR7', 'Librairie Gelistout', 102);
+        echo $p;
+        echo '<br>';
+        echo "Nom du projet = " . $p->getNomProjet();
+    }
+
+    public static function instanciationEmployeInformaticienV1(): void {
+        $p = new Projet('PR7', 'Librairie Gelistout', 102);
+        echo $p;
+        $informaticien = new EmployeInformaticien(1, "Dupont", "Jacques", new DateTime("12/07/1980"), 1000.00, 0.0, $p);
+        echo "<p>";
+        echo $informaticien;
+        echo "<p>";
+        $informaticien->setPrimeM(600);
+        echo "Nouvelle prime = " . $informaticien->getPrimeM();
+    }
+
+    public static function instanciationEmployeNonInformaticienV1(): void {
+        $ninformaticien = new EmployeNonInformaticien(5, "Roman", "Denis", new DateTime("07/02/1984"), 13000.00, 0.0);
+        echo "<p>";
+        echo $ninformaticien;
+        echo "<p>";
+        $ninformaticien->setPrimeA(14000);
+        echo "Nouvelle prime = " . $ninformaticien->getPrimeA();
+    }
+
+    /* ->getNom();
+      echo "l'employé instancié s'appelle " . $nom;
+     */
 }
--- index.php
+++ index.php
@@ -3,22 +3,25 @@
 Page index.php fait par LAUDE Thibault
 -->
 <html>
-  <head>
-    <meta charset="UTF-8">
-    <title></title>
-  </head>
-  <body>
-    <?php
-    include 'classes/employe.php';
-    include 'includes/Traitement.php';
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        include 'classes/employe.php';
+        include 'classes/projet.php';
+        include 'classes/employeinformaticien.php';
+        include 'classes/employenoninformaticien.php';
+        include 'includes/Traitement.php';
 
-    try {
-      Traitement::instanciationUnEmploye();
-      Traitement::instanciationUnEmployeErreur();
-      Traitement::instanciationUnEmploye();
-    } catch (Exception $ex) {
-      echo $ex->getMessage();
-    }
-    ?>
-  </body>
+        try {
+            Traitement::instanciationProjetV1();
+            Traitement::instanciationEmployeInformaticienV1();
+            Traitement::instanciationEmployeNonInformaticienV1();
+        } catch (Exception $ex) {
+            echo $ex->getMessage();
+        }
+        ?>
+    </body>
 </html>
