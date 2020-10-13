0be0775f0e8b881bb131235a791d7949f8c9ba87
Thibault Laude <laudethibault_pro@outlook.fr>
2020-10-13 20:39:43 +0200

    Implémentation de la classe Employe Non Informaticien Task #7 - Implémenter la classe Employe Non Informaticien

---
+++ classes/EmployeNonInformaticien.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Description de la classe EmployeNonInformaticien
+ *
+ * @author Thibault
+ */
+class EmployeNonInformaticien {
+
+    private float $primeA;
+
+    public function __construct(string $pnumero, string $pnom, string $pprenom, DateTime $pdate, float $psalaire, float $pprimeA) {
+        Employe::__construct($pnumero, $pnom, $pprenom, $pdate, $psalaire);
+        $this->primeA = $pprimeA;
+    }
+
+    public function setPrimeA(float $primeA): void {
+        if ($primeA > $this->getSalaireM()) {
+            $this->primeA = $this->getSalaireM();
+        } elseif ($primeA < $this->getSalaireM()) {
+            $this->primeA = $primeA;
+        }
+    }
+
+    public function getPrimeA(): float {
+        return $this->primeA;
+    }
+
+    public function __toString(): string {
+        return "Non Informaticien : " . $this->numero . " - " . $this->getNom() . " - " . $this->getPrenom() . " - "
+                . $this->dateDeNaissance->format('d/m/Y') . " - " . $this->getSalaireM() . $this->getPrimeA();
+    }
+
+}
