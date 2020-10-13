76c17e16c508fb045376a0acae4aa868b8b1d2d8
Thibault Laude <laudethibault_pro@outlook.fr>
2020-10-13 20:35:12 +0200

    Correction employe

--- classes/Employe.php
+++ classes/Employe.php
@@ -6,54 +6,53 @@
  * @author laude.thibault
  */
 class Employe {
-
-  protected int $numero;
-  protected string $nom;
-  protected string $prenom;
-  protected DateTime $dateDeNaissance;
-  protected float $salaireM;
-  private const MINSALAIRE = 1000;
-
-  public function __construct(int $pnum, string $pnom, string $pprenom, DateTime $pddn, float $psalaire) {
-    $this->numero = $pnum;
-    $this->nom = $pnom;
-    $this->prenom = $pprenom;
-    $this->dateDeNaissance = $pddn;
-    $this->setSalaireM($psalaire);
-  }
-
-  public function getNumero(): int {
-    return $this->numero;
-  }
-
-  public function getNom(): string {
-    return $this->nom;
-  }
-
-  public function getPrenom(): string {
-    return $this->prenom;
-  }
-
-  function getDateDeNaissance(): DateTime {
-    return $this->dateDeNaissance;
-  }
-
-  function getSalaireM(): float {
-    return $this->salaireM;
-  }
-  
-  public function setSalaireM(float $salaireM): void {
-    if($salaireM >= self::MINSALAIRE){
-      $this->salaireM=$salaireM;
+    protected int $numero;
+    protected string $nom;
+    protected string $prenom;
+    protected DateTime $dateDeNaissance;
+    protected float $salaireM;
+    private const MINSALAIRE = 1000;
+    
+    public function __construct(int $pnumero, string $pnom, string $pprenom, DateTime $pddn, float $psalaireM) {
+        $this->numero = $pnumero;
+        $this->nom = $pnom;
+        $this->prenom = $pprenom;
+        $this->dateDeNaissance = $pddn;
+        $this->setSalaireM($psalaireM);
     }
-    else{
-      throw new Exception(" | le salaire ne doit pas être inférieur à " . self::MINSALAIRE);
+    
+    public function getNumero(): int {
+        return $this->numero;
     }
-  }
-  
-  public function __toString(): string {
-    return $this->numero . " - " .$this->getNom() . " - " .$this->getPrenom(). " - "
-            . $this->dateDeNaissance->format('d/m/Y') . " - ". $this->getSalaireM();
-  }
+
+    public function getNom(): string {
+        return $this->nom;
+    }
+
+    public function getPrenom(): string {
+        return $this->prenom;
+    }
+
+    public function getDateDeNaissance(): DateTime {
+        return $this->dateDeNaissance;
+    }
+
+    function getSalaireM(): float {
+        return $this->salaireM;
+    }
+
+    function setSalaireM(float $salaireM): void {
+        if ($salaireM >= self::MINSALAIRE) {
+            $this->salaireM = $salaireM;
+        }
+        else {
+            throw new Exception('Le salaire ne peu pas etre inferieur a ' .self::MINSALAIRE);
+        }
+    }
+
+    public function __toString(): string {
+        return $this->numero . " - " . $this->getNom() . " - " . $this->getPrenom(). " - "
+                . $this->dateDeNaissance->format('d/m/Y') . " - ". $this->getSalaireM();
+    }
 
 }
